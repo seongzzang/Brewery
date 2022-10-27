03deaf4c4ddcc1f993db4d135c1162dfa3a903ed
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-10-27 23:10:18 +0900

    setting_beerlistcell

--- Brewery/BeerListVIewController.swift
+++ Brewery/BeerListVIewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class BeerListViewController: UITableViewController {
     
+    var beerList = [Beer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        //UINavigationBar
+        title = "브루어리"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //UITableView 설정
+        tableView.register(BeerListCell.self, forCellReuseIdentifier: "BeerListCell")
+        tableView.rowHeight = 150
     }
     
+}
+
+//UITableView DataSource, Delegate
+extension BeerListViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beerList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BeerListCell", for:indexPath) as? BeerListCell else {return UITableViewCell()}
+        
+        let beer =  beerList[indexPath.row]
+        cell.configure(with: beer)
+        
+        return cell
+    }
 }
