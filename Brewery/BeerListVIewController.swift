cc4c995baec5d81d36a3a3d535e817ebb4e64ca1
seongzzang <87599027+seongzzang@users.noreply.github.com>
2022-10-27 22:42:57 +0900

    setting_Beer_Data

---
+++ Brewery/BeerListVIewController.swift
@@ -0,0 +1,18 @@
+//
+//  BeerListVIewController.swift
+//  Brewery
+//
+//  Created by 양성혜 on 2022/10/27.
+//
+
+import UIKit
+
+
+class BeerListViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+}
